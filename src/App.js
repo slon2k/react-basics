d1ad087473417769abe56a3bb91f543da9d977fe
Boris Korobeinikov <slon2k@gmail.com>
2019-06-01 20:43:40 +0600

    Add date

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div>
-      Project 1
-    </div>
-  );
+export default class App extends Component {
+   state = {
+       time: ''
+   };
+
+   updateTime = () => {
+       this.setState({time: Date()});
+   };
+
+   componentDidMount() {
+       this.updateTime();
+       this.interval = setInterval(this.updateTime, 1000);
+   }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render () {
+      const {time} = this.state;
+      return (
+          <div>
+              <h1>Project 1</h1>
+              <h3>{time}</h3>
+          </div>
+      )
+  }
 }
 
-export default App;
