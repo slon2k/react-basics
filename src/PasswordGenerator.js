cab70964ee02b86e396726c9a33b268ea359217e
Boris Korobeinikov <slon2k@gmail.com>
2019-06-04 11:21:07 +0600

    refactor Generator

--- src/PasswordGenerator.js
+++ src/PasswordGenerator.js
@@ -3,134 +3,90 @@ import React, {Component} from 'react'
 export default class PasswordGenerator extends Component {
 
     state = {
-
-    };
-
-    setInitialState() {
-        const state = {
-            pass: '',
-            fields: [
-                {fieldName: 'Length', type: 'range', value: 8, readonly: false, min: 6, max: 12},
-                {fieldName: 'Numbers', type: 'checkbox', value: true, readonly: true},
-                {fieldName: 'Lowercase', type: 'checkbox', value: false, readonly: false},
-                {fieldName: 'Uppercase', type: 'checkbox', value: false, readonly: false},
-                {fieldName: 'Symbols', type: 'checkbox', value: false, readonly: false}
-            ]
-        };
-
-        this.setState(state);
+        pass: '',
+        length: 8,
+        sets: {
+            Numbers: {name: "Numbers", checked: true, readonly: true, charSet: "0123456789"},
+            Lowercase: {name: "Lowercase", checked: false, readonly: false, charSet: "abcdefghijklmnopqrstuvwxyz"},
+            Uppercase: {name: "Uppercase", checked: false, readonly: false, charSet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
+            Symbols: {name: "Symbols", checked: false, readonly: false, charSet: "!#$%&()*+,-./:;<=>?@[]^_{|}~"},
+        }
     }
 
-    componentWillMount() {
-        this.setInitialState()
-    }
-
-    createRange = (item) => {
+    createRange() {
+        const {length} = this.state;
         return(
-            <div key = {item.fieldName}>
+            <div key = {"length"}>
                 <label>
-                    {item.fieldName}
+                    Length
                     <input
-                        key = {item.fieldName}
-                        onChange = {this.handleChangeRange}
-                        type={item.type}
-                        name={item.fieldName}
-                        value={item.value}
-                        readOnly={item.readonly}
-                        min={item.min}
-                        max={item.max}/>
+                        onChange = {this.changeLength}
+                        type="Range"
+                        value={length}
+                        min="6"
+                        max="12"/>
                 </label>
-                {item.value}
+                {length}
                 <br/>
             </div>
         )
-    };
+    }
 
-    handleChangeRange = (event) => {
-        const {fields} = this.state;
-        const index = fields.findIndex((item) => item.fieldName === event.target.name);
-        if (index > -1) {
-            fields[index].value = event.target.value;
-            this.setState({fields})
+    changeLength = (event) => {this.setState({length: event.target.value})}
+
+    toggleSet = (set) => {
+        const {sets} = this.state;
+        if(!sets[set].readonly) {
+            sets[set].checked = !sets[set].checked;
+            this.setState({sets});
         }
-    };
-
-    handleCheck = (event) => {
-        const {fields} = this.state;
-        const index = fields.findIndex((item) => item.fieldName === event.target.name);
-        if (index > -1 && !fields[index].readonly)  {
-            fields[index].value = event.target.checked;
-            this.setState({fields})
-        }
-    };
-
-    createCheckbox = (item) => {
-        return(
-            <div key = {item.fieldName}>
-                <label>
-                    <input
-                        onChange = {this.handleCheck}
-                        type={item.type}
-                        name={item.fieldName}
-                        checked={item.value}
-                        readOnly={item.readonly}/>
-                    {item.fieldName}
-                </label>
-                <br/>
-            </div>
-        )
-    };
+    }
 
     getRandomSymbol = (str) => {
         const index = Math.floor(Math.random()*str.length);
         return str[index];
-    };
+    }
 
     generatePassword = (event) => {
+        const {sets, length} = this.state;
+        const charSet = Object.values(sets)
+            .filter((item) => {return item.checked})
+            .map((item) => {return item.charSet})
+            .join('');
 
-        const sets = {
-                Numbers: "0123456789",
-                Lowercase: "abcdefghijklmnopqrstuvwxyz",
-                Uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                Symbols: "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
-        };
-
-        let charSet = '';
-
-        let length = 0;
-
-        const {fields} = this.state;
-
-        for (let i = 0; i < fields.length; i++) {
-            const field = fields[i];
-            if (field.type === 'checkbox' && field.value === true) {
-                charSet = charSet + sets[field.fieldName];
-            }
-            if (field.fieldName === "Length"){
-                length = field.value;
-            }
-        }
-
-        let newPass = '';
+        let newPass = [''];
 
         for (let i = 0; i < length; i++) {
-            newPass = `${newPass}${this.getRandomSymbol(charSet)}`;
+            newPass.push(this.getRandomSymbol(charSet));
         }
 
-        this.setState({pass: newPass});
+        this.setState({pass: newPass.join('')});
 
         event.preventDefault();
     };
 
-    render() {
-        const {pass, fields} = this.state;
+    createFields(sets) {
+        return Object.values(sets).map((item) => {
+            return(
+                <div key={item.name}>
+                    <label>
+                        <input
+                            onChange = {() => this.toggleSet(item.name)}
+                            type="checkbox"
+                            name={item.name}
+                            checked={item.checked}
+                            readOnly={item.readonly}/>
+                        {item.name}
+                    </label>
+                    <br/>
+                </div>
+            )
+        })
+    }
 
-        const formFields = fields.map((item) => {
-            if(item.type === 'range') {
-                return this.createRange(item)
-            }
-            return this.createCheckbox(item)
-        });
+    render() {
+        const {pass, sets} = this.state;
+        const fields = this.createFields(sets);
 
         return(
             <div>
@@ -139,9 +95,10 @@ export default class PasswordGenerator extends Component {
                 <div>
                     <input type="text" readOnly={true} value={pass}/>
                 </div>
+                {this.createRange()}
 
                 <form>
-                    {formFields}
+                    {fields}
                     <button onClick={this.generatePassword}>Generate</button>
                 </form>
             </div>
