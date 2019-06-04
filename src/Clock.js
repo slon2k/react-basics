77a1f46bbc6e22c21d8026efe333152451f118b4
Boris Korobeinikov <slon2k@gmail.com>
2019-06-04 14:27:16 +0600

    methods outside class

--- src/Clock.js
+++ src/Clock.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react';
 
+const toTwoDigits = (num) => {
+    return parseInt(num).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+}
+
+const time24 = (time) => {
+    return `${time.getHours()}:${toTwoDigits(time.getMinutes())}:${toTwoDigits(time.getSeconds())}`;
+}
+
+const time12 = (time) => {
+    let hours = time.getHours();
+    const dayTime = hours < 12 ? "AM" : "PM";
+    hours = hours % 12;
+    hours = hours > 0 ? hours : 12;
+    return `${hours}:${toTwoDigits(time.getMinutes())}:${toTwoDigits(time.getSeconds())} ${dayTime}`;
+}
+
+
 export default class Clock extends Component {
     state = {
         mode: '24_hours',
@@ -14,22 +31,6 @@ export default class Clock extends Component {
         this.setState({mode});
     }
 
-    toTwoDigits = (num) => {
-        return parseInt(num).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    }
-
-    time24 = (time) => {
-        return `${time.getHours()}:${this.toTwoDigits(time.getMinutes())}:${this.toTwoDigits(time.getSeconds())}`;
-    }
-
-    time12 = (time) => {
-        let hours = time.getHours();
-        const dayTime = hours < 12 ? "AM" : "PM";
-        hours = hours % 12;
-        hours = hours > 0 ? hours : 12;
-        return `${hours}:${this.toTwoDigits(time.getMinutes())}:${this.toTwoDigits(time.getSeconds())} ${dayTime}`;
-    }
-
     componentDidMount() {
         this.interval = setInterval(this.updateTime, 1000);
     }
@@ -40,7 +41,7 @@ export default class Clock extends Component {
 
     render () {
         const {time, mode} = this.state;
-        const formattedTime = mode === '24_hours' ? this.time24(time) :  this.time12(time);
+        const formattedTime = mode === '24_hours' ? time24(time) :  time12(time);
         return (
             <div>
                 <h2>Digital clock</h2>
