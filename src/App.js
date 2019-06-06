007185197dad653f11b947a8ee7d7a8cf080a2b7
Boris Korobeinikov <slon2k@gmail.com>
2019-06-06 14:58:08 +0600

    Rating only

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Clock from './Clock';
-import PasswordGenerator from './PasswordGenerator'
+//import Clock from './Clock';
+//import PasswordGenerator from './PasswordGenerator'
 import Rating from './NewRating'
 
 const App =  () => {
     return (
         <div>
             <h1>React Basics</h1>
-            <Clock/>
-            <hr/>
-            <PasswordGenerator/>
             <hr/>
             <Rating max={5}/>
 
